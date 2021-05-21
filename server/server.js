01841cafb7e219f1d0189486724668d04a456151
Eric Knechtges <eric.t.knechtges@gmail.com>
2021-05-20 23:10:11 -0400

    Finished initial restructuring

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 console.log(__dirname);
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/../client/public'));
 app.use(cors());
 
 /*app.get('/', (req, res) => {
