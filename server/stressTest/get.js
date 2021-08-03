6a18cb225495b801cd991572db85cee435e67607
VirtCoder <joekic@gmail.com>
2021-08-02 20:30:59 -0400

    setup K6 & New Relic

---
+++ server/stressTest/get.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export let options = {
+    scenarios: {
+      constant_request_rate: {
+        executor: "constant-arrival-rate",
+        rate: 10000,
+        timeUnit: "1s",
+        duration: "120s",
+        preAllocatedVUs: 100,
+        maxVUs: 300,
+      },
+    }
+  };
+
+export default function () {
+    const testUrl = 'http://localhost:5000/?courseId=9123456';
+    const response = http.get(testUrl);
+    check(response, {
+        'response status 200': (r) => r.status === 200,
+    });
+}
